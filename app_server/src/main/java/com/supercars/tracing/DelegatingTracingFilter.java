91d67e5a5ffdc93e22fddfb938bfb3fa86d427df
Tom Batchelor <tom@observeinc.com>
2019-09-13 10:22:53 -0700

    Added Basic Tracing with Blaze, sending to a local Zipkin. Also MySQL driver to V8 and Java to V12
    MySQL Driver to V8 for modernization.
    
    Note that this does not fully cover all app operation with tracing, just captures incoming HTTP naming simply with the method and just downstream SQL calls
    
    JAXB is add to the POM as it's now not on the classpath for later JDKs

---
+++ app_server/src/main/java/com/supercars/tracing/DelegatingTracingFilter.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.supercars.tracing;
+
+import brave.Tracing;
+import brave.servlet.TracingFilter;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Sender;
+import zipkin2.reporter.urlconnection.URLConnectionSender;
+
+/**
+ *
+ * @author tombatchelor
+ */
+public class DelegatingTracingFilter implements Filter {
+  Sender sender = URLConnectionSender.create("http://127.0.0.1:9411/api/v2/spans");
+  AsyncReporter<Span> spanReporter = AsyncReporter.create(sender);
+  Tracing tracing = Tracing.newBuilder()
+        .localServiceName("cars-app")
+        .spanReporter(spanReporter).build();
+  Filter delegate = TracingFilter.create(tracing);
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    delegate.doFilter(request, response, chain);
+  }
+
+  @Override public void destroy() {
+    try {
+      tracing.close(); // disables Tracing.current()
+      spanReporter.close(); // stops reporting thread and flushes data
+      sender.close(); // closes any transport resources
+    } catch (IOException e) {
+      // do something real
+    }
+  }
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+        return;
+    }
+}
