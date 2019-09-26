220c0ebe581de17c811f33bd12b98a1763974fac
Tom Batchelor <tom@observeinc.com>
2019-09-26 08:38:08 -0700

    Updated clean up cron to remove cars to 200 below the limit

--- cron_cleanup/clean_up.py
+++ cron_cleanup/clean_up.py
@@ -8,14 +8,14 @@ def get_array_of_randoms(length, limit):
     randoms = list(dict.fromkeys(randoms))
     return randoms
 
-def trim_table(selectQuery, deleteQuery, max, cnx):
+def trim_table(selectQuery, deleteQuery, max, toDelete, cnx):
     cursor = cnx.cursor()
     cursor.execute(selectQuery)
     IDs = cursor.fetchall()
     cursor.close()
     
     if len(IDs) > max:
-        randoms = get_array_of_randoms(len(IDs) - max, len(IDs))
+        randoms = get_array_of_randoms(len(IDs) - max + toDelete, len(IDs))
         print(len(randoms))
         cursor = cnx.cursor()
         # Don't delete car 1 as that is where enquiries get posted
@@ -39,15 +39,17 @@ enquiryQuery = 'SELECT ENQUIRY_ID FROM ENQUIRIES'
 enquiryDelete = 'DELETE FROM ENQUIRIES WHERE ENQUIRY_ID = %s'
 
 maxCars = 3000
-maxQueries = 3000
+carsToDelete = 2000
+maxEnquiries = 3000
+enquiriesToDelete = 2000
 
 print('Start Cleanup')
 cnx = mysql.connector.connect(**config)
 
-trim_table(carsQuery, carsDelete, maxCars, cnx)
+trim_table(carsQuery, carsDelete, maxCars, carsToDelete, cnx)
 print('End CARS cleanup')
 print('Start ENQUIRIES cleanup')
-trim_table(enquiryQuery, enquiryDelete, maxQueries, cnx)
+trim_table(enquiryQuery, enquiryDelete, maxEnquiries, enquiriesToDelete, cnx)
 print('End ENQUIRIES cleanup')
 
 cnx.close()
