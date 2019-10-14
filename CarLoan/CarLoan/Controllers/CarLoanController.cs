742f649ab32f7e8208128616884ad09b91d598e4
Tom Batchelor <tom@observeinc.com>
2019-10-14 09:41:07 -0700

    Initial bare bones Car Loan service

---
+++ CarLoan/CarLoan/Controllers/CarLoanController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CarLoan.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarLoanController : ControllerBase
+    {
+
+        private readonly ILogger<CarLoanController> _logger;
+
+        public CarLoanController(ILogger<CarLoanController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public LoanQuote Get()
+        {
+            var quote = new LoanQuote();
+            quote.payment = 349.34F;
+            quote.rate = 3.99F;
+            quote.term = 60;
+            return quote;
+        }
+    }
+}
---
+++ CarLoan/CarLoan/LoanQuote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CarLoan
+{
+    public class LoanQuote
+    {
+        public float rate { get; set; }
+        public float payment { get; set; }
+        public int term { get; set; }
+
+        public LoanQuote()
+        {
+        }
+    }
+}
