389a570af53ac32e7fd222f6444b3c03015f1f2b
Tom Batchelor <tom@observeinc.com>
2020-02-03 14:55:52 -0800

    Adding metadata tools

---
+++ demo_marshaller/applyData.sh
@@ -0,0 +1,2 @@
+curl  -X POST -H 'Content-Type: application/json' -H 'Authorization: Bearer 108237415577 rCUpCy2Lgfbifvhb2UtWKw3bTKERQvJQ' -d @HTTPCodes.json https://collect.observe-staging.com/v1/observations/httpcodes
+curl  -X POST -H 'Content-Type: application/json' -H 'Authorization: Bearer 108237415577 rCUpCy2Lgfbifvhb2UtWKw3bTKERQvJQ' -d @users.json https://collect.observe-staging.com/v1/observations/users
