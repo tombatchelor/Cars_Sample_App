dd8b369aba88c5bb56cbd8b43dc1737d207b199f
Tom Batchelor <tom@observeinc.com>
2020-03-23 15:28:35 -0700

    Migrate to RDS

--- app_server/setenv.sh
+++ app_server/setenv.sh
@@ -1,1 +1,1 @@
-export JAVA_OPTS="$JAVA_OPTS -Ddatabase.address=$DATABASE_ADDRESS -Ddatabase.password=$DATABASE_PASSWORD -Ddatabase.username=$DATABASE_USERNAME"
+export JAVA_OPTS="$JAVA_OPTS -Ddatabase.password=$DATABASE_PASSWORD -Ddatabase.username=$DATABASE_USERNAME"
