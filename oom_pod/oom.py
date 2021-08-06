2bc49565ae9b966c8635178d4ad32c9f21e52bdd
tombatchelor <tom@observeinc.com>
2021-08-06 09:13:49 -0700

    Updates to OOM config

--- oom_pod/oom.py
+++ oom_pod/oom.py
@@ -3,7 +3,8 @@ import time
 array = []
 while True:
   innerArray = []
-  for x in range(100000):
+  for x in range(25000):
     innerArray.append("This is a string I'm adding to an array")
+  print("OOM: I'm appending 25000 items to the array", flush = True)
   array.append(innerArray)
   time.sleep(1)
