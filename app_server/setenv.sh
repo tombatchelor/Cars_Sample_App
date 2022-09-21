5e743fa2b1c1b8fb7dd3f74595f854848161d1f1
Tom Batchelor <tom@observeinc.com>
2022-09-21 10:40:46 -0700

    Added bigger heap, larger than the Pod def has

--- app_server/setenv.sh
+++ app_server/setenv.sh
@@ -1,1 +1,1 @@
-export JAVA_OPTS="$JAVA_OPTS -Ddatabase.password=$DATABASE_PASSWORD -Ddatabase.username=$DATABASE_USERNAME"
+export JAVA_OPTS="$JAVA_OPTS -Ddatabase.password=$DATABASE_PASSWORD -Ddatabase.username=$DATABASE_USERNAME -Xmx1024m"
